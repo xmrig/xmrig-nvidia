cde3f9806cda769c952aaff34184c06d17f83e00
XMRig <support@xmrig.com>
2018-07-27 07:30:36 +0700

    #184 Fixed build without NVML API.

--- src/nvidia/NvmlApi_stub.cpp
+++ src/nvidia/NvmlApi_stub.cpp
@@ -4,8 +4,7 @@
  * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
- * Copyright 2016-2017 XMRig       <support@xmrig.com>
- *
+ * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com> *
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -39,7 +38,7 @@ void NvmlApi::release()
 }
 
 
-bool NvmlApi::health(int id, Health &health)
+bool NvmlApi::health(int, Health &)
 {
     return false;
 }
@@ -51,6 +50,6 @@ const char *NvmlApi::version()
 }
 
 
-void NvmlApi::bind(const std::vector<GpuThread*> &threads)
+void NvmlApi::bind(const std::vector<xmrig::IThread*> &)
 {
 }
