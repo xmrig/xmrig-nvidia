667ce46d6d614cbc05714cbc14368f39aa3b6edc
XMRig <support@xmrig.com>
2019-02-08 16:11:58 +0700

    Sync changes.

--- src/net/strategies/DonateStrategy.cpp
+++ src/net/strategies/DonateStrategy.cpp
@@ -5,7 +5,8 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -95,6 +96,12 @@ void DonateStrategy::connect()
 }
 
 
+void DonateStrategy::setAlgo(const xmrig::Algorithm &algo)
+{
+    m_strategy->setAlgo(algo);
+}
+
+
 void DonateStrategy::stop()
 {
     uv_timer_stop(&m_timer);
@@ -128,7 +135,9 @@ void DonateStrategy::onActive(IStrategy *strategy, Client *client)
 
 void DonateStrategy::onJob(IStrategy *strategy, Client *client, const Job &job)
 {
-    m_listener->onJob(this, client, job);
+    if (isActive()) {
+        m_listener->onJob(this, client, job);
+    }
 }
 
 
--- src/net/strategies/DonateStrategy.h
+++ src/net/strategies/DonateStrategy.h
@@ -5,7 +5,8 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -52,6 +53,7 @@ public:
 
     int64_t submit(const JobResult &result) override;
     void connect() override;
+    void setAlgo(const xmrig::Algorithm &algo) override;
     void stop() override;
     void tick(uint64_t now) override;
 
