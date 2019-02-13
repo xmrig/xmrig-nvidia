93eb05f42a25d90e31b2f55bed39a483ecdce685
XMRig <support@xmrig.com>
2019-02-14 06:04:13 +0700

    v2.12.1-dev

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@
 #define APP_ID        "xmrig-nvidia"
 #define APP_NAME      "XMRig-NVIDIA"
 #define APP_DESC      "XMRig CUDA miner"
-#define APP_VERSION   "2.12.0-beta"
+#define APP_VERSION   "2.12.1-dev"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2018 xmrig.com"
@@ -36,7 +36,7 @@
 
 #define APP_VER_MAJOR  2
 #define APP_VER_MINOR  12
-#define APP_VER_PATCH  0
+#define APP_VER_PATCH  1
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1910)
