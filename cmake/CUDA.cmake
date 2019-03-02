4ee84fea428b77ee1c47423736c49954733e4d36
XMRig <support@xmrig.com>
2019-03-02 18:09:18 +0700

    #241 Increase minimum required CUDA version to 8.0 due CUDA 7.5 lacks support for "nvrtcAddNameExpression".

--- cmake/CUDA.cmake
+++ cmake/CUDA.cmake
@@ -21,7 +21,7 @@ list(APPEND CMAKE_PREFIX_PATH "$ENV{CUDA_ROOT}")
 list(APPEND CMAKE_PREFIX_PATH "$ENV{CMAKE_PREFIX_PATH}")
 
 set(CUDA_STATIC ON)
-find_package(CUDA 7.5 REQUIRED)
+find_package(CUDA 8.0 REQUIRED)
 
 find_library(CUDA_LIB libcuda cuda HINTS "${CUDA_TOOLKIT_ROOT_DIR}/lib64" "${LIBCUDA_LIBRARY_DIR}" "${CUDA_TOOLKIT_ROOT_DIR}/lib/x64" /usr/lib64 /usr/local/cuda/lib64)
 find_library(CUDA_NVRTC_LIB libnvrtc nvrtc HINTS "${CUDA_TOOLKIT_ROOT_DIR}/lib64" "${LIBNVRTC_LIBRARY_DIR}" "${CUDA_TOOLKIT_ROOT_DIR}/lib/x64" /usr/lib64 /usr/local/cuda/lib64)
